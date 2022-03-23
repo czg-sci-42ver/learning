dd4148822d78532ef378756fa01c6b21acfe8706
czg <net1_6_3ease@163.com>
2022-03-23 14:57:34 +0800

    add nest_exception/ swap_with_friend_&_overload/build/Capacity

--- swap_with_friend_&_overload/src/Capacity.cpp
+++ swap_with_friend_&_overload/src/Capacity.cpp
@@ -1,31 +1,38 @@
 #include <algorithm>
 #include <iostream>
- 
-namespace Ns {
-class A {
-    int id{};
- 
-    friend void swap(A& lhs, A& rhs) {
-        std::cout << "swap(" << lhs << ", " << rhs << ")\n";
-        std::swap(lhs.id, rhs.id);
-    }
- 
-    friend std::ostream& operator<< (std::ostream& os, A const& a) {
-        return os << "A::id=" << a.id;
-    }
- 
-public:
-    A(int i) : id(i) { }
-    // A(A const&) = delete;
-    
-    // A& operator = (A const&) = delete;
 
-    // A();
+namespace Ns {
+class A
+{
+  int id{};
+
+  friend void swap(A& lhs, A& rhs)
+  {
+    std::cout << "swap(" << lhs << ", " << rhs << ")\n";
+    std::swap(lhs.id, rhs.id);
+  }
+
+  friend std::ostream& operator<<(std::ostream& os, A const& a)
+  {
+    return os << "A::id=" << a.id;
+  }
+
+public:
+  A(int i)
+    : id(i)
+  {
+  }
+  // A(A const&) = delete;
+
+  // A& operator = (A const&) = delete;
+
+  // A();
 };
 }
 
 namespace Ns_child {
-class A_child :public Ns::A{
+class A_child : public Ns::A
+{
   // int id{10};
   // public:
   // A_child(int i) : id(i) { };
@@ -33,25 +40,37 @@ class A_child :public Ns::A{
 };
 }
 
- 
-int main()
+int
+main()
 {
-    int a = 5, b = 3;
-    std::cout << a << ' ' << b << '\n';
-    std::swap(a,b);
-    std::cout << a << ' ' << b << '\n';
- 
-    Ns::A p{6}, q{9};
-    std::cout << p << ' ' << q << '\n';
-//  std::swap(p, q);  // error, type requirements are not satisfied
-    swap(p, q);  // OK, ADL finds the appropriate friend `swap`
-    std::cout << p << ' ' << q << '\n';
+  int a = 5, b = 3;
+  std::cout << a << ' ' << b << '\n';
+  std::swap(a, b);
+  std::cout << a << ' ' << b << '\n';
 
-    // Ns_child::A_child test(3); //weird can't initialize
-    // see inheritance... directory at top level of git repo
-    // which also test friend function usage
+  Ns::A p{ 6 }, q{ 9 };
+  std::cout << p << ' ' << q << '\n';
+  //  std::swap(p, q);  // error, type requirements are not satisfied
+  swap(p, q); // OK, ADL finds the appropriate friend `swap`
+  std::cout << p << ' ' << q << '\n';
 
+  // Ns_child::A_child test(3); //weird can't initialize
+  // see inheritance... directory at top level of git repo
+  // which also test friend function usage
 
-    // std::cout << " czg_test " << test << '\n' << std::endl;
-    // test = p;
+  // std::cout << " czg_test " << test << '\n' << std::endl;
+  // test = p;
+  int i =1;
+  switch (i) {
+    case 1:
+      // int x = 0; // initialization
+      std::cout << "t" << '\n';
+      std::cout << "t" << '\n';
+      std::cout << "t" << '\n'; 
+      //  [[fallthrough]];
+    default: // compilation error: jump to default: would enter the scope of 'x'
+      // without initializing it
+      std::cout << "default\n";
+      break;
+  }
 }
