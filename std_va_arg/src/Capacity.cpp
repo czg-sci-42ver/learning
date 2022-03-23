2b8bdfc9262524cb9ca22805c205dc567b31d45b
czg <net1_6_3ease@163.com>
2022-03-23 21:04:25 +0800

    add std_va_arg/ std_variant/

---
+++ std_va_arg/src/Capacity.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+#include <cstdarg>
+#include <cmath>
+ 
+double stddev(int count, ...) 
+{
+    double sum = 0;
+    double sum_sq = 0;
+    std::va_list args;
+    va_start(args, count);
+    double test_1 = va_arg(args, double);
+    std::cout << " czg_test " << test_1 << '\t' << std::endl;
+    // va_end(args);
+    double test = va_arg(args, double);
+    std::cout << " czg_test " << test << '\t' << std::endl;
+    for (int i = 0; i < count; ++i) {
+        double num = va_arg(args, double);
+        sum += num;
+        sum_sq += num*num;
+    }
+    
+    return std::sqrt(sum_sq/count - (sum/count)*(sum/count));
+}
+ 
+int main() 
+{
+    std::cout << stddev(3, 25.0, 27.3, 26.9, 25) << '\n';
+}
